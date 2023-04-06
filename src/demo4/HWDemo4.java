7f4801dead4baa3c2a7ba4f45e427037fc77d29a
ian_c <ian_c@Thetool>
2023-04-05 23:18:40 -0400

    Initial Commit

---
+++ src/demo4/HWDemo4.java
@@ -0,0 +1,16 @@
+package demo4;
+
+public class HWDemo4 {
+	
+	public static void main(String[] args) {
+		System.out.println("Test");
+		
+		
+		
+		
+		System.out.println("GitHub HW");
+	}
+	
+	
+
+}
